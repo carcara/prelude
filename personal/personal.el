4b7fa16eb50f78707272b132536d4800d27cf5e8
Renato Fernandes de Queiroz <renatofq@gmail.com>
2013-09-30 23:38:48 -0300

    Personal settings.

---
+++ personal/personal.el
@@ -0,0 +1,17 @@
+;;; personal.el --- personal setup
+
+;;; Code:
+
+;;; Theme setup
+(disable-theme 'zenburn)
+(load-theme 'solarized-dark t)
+
+(projectile-global-mode)
+
+(add-hook 'java-mode-hook
+          (lambda ()
+            (setq c-basic-offset 2
+                  tab-width 2
+                  indent-tabs-mode nil)))
+
+;;; personal.el ends here
