9a792b6b237f3071bcbd3ef0691d2170877e45ee
Renato Fernandes de Queiroz <renatofq@gmail.com>
2015-08-08 09:30:14 -0300

    nothing relevant

--- personal/personal.el
+++ personal/personal.el
@@ -4,12 +4,6 @@
 (load-theme 'ample t)
 (scroll-bar-mode -1)
 
-(autoload 'cobol-mode "cobol-mode" "A major mode for editing ANSI Cobol/Scobol files." t nil)
-
-;; Emacs Speaks Statistics
-(setq load-path (cons "/usr/share/emacs/site-lisp/ess" load-path))
-(require 'ess-site)
-
 (setq org-log-done nil)
 (setq prelude-flyspell nil)
 
@@ -18,20 +12,25 @@
 
 (projectile-global-mode)
 
-(setq ediff-split-window-function 'split-window-horizontally)
 
+(autoload 'cobol-mode "cobol-mode"
+  "A major mode for editing ANSI Cobol/Scobol files." t nil)
+
+(autoload 'plantuml-mode "plantuml-mode"
+  "A major mode for editing plantuml files." t nil)
+
+;; Emacs Speaks Statistics
+(setq load-path (cons "/usr/share/emacs/site-lisp/ess" load-path))
+(require 'ess-site)
+
+
+(setq ediff-split-window-function 'split-window-horizontally)
 
 (add-hook 'c-mode-hook
           (lambda ()
             (setq c-default "bsd"
                   c-basic-offset 2
-                  tab-width 2
                   indent-tabs-mode t)
             (smart-tabs-mode-enable)
             (smart-tabs-advice c-indent-line c-basic-offset)
             (smart-tabs-advice c-indent-region c-basic-offset)))
-
-(add-hook 'lua-mode-hook
-          (lambda ()
-            (setq lua-indent-level 2
-                  tab-width 2)))
