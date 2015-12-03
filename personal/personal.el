f31fce5c5ab4ae8fc28f88a64ae3c67ec14a4072
Renato Fernandes de Queiroz <renatofq@gmail.com>
2015-12-03 08:49:55 -0200

    Enable company-mode, disable cobol-mode autoload, disable octave-mode autoload, disable ess-site, cleanup c-mode-hook.

--- personal/personal.el
+++ personal/personal.el
@@ -1,7 +1,5 @@
 ;; Theme setup
 (setq default-frame-alist '((font . "Source Code Pro-11")))
-(disable-theme 'zenburn)
-(load-theme 'ample t)
 (scroll-bar-mode -1)
 
 (setq org-log-done nil)
@@ -12,25 +10,29 @@
 
 (projectile-global-mode)
 
+(add-hook 'after-init-hook 'global-company-mode)
 
-(autoload 'cobol-mode "cobol-mode"
-  "A major mode for editing ANSI Cobol/Scobol files." t nil)
+;; (autoload 'cobol-mode "cobol-mode"
+;;   "A major mode for editing ANSI Cobol/Scobol files." t nil)
 
-(autoload 'plantuml-mode "plantuml-mode"
-  "A major mode for editing plantuml files." t nil)
+;; (autoload 'plantuml-mode "plantuml-mode"
+;;   "A major mode for editing plantuml files." t nil)
+
+;; (autoload 'octave-mode "octave-mode" nil t)
+;; (setq auto-mode-alist
+;;       (cons '("\\.m$" . octave-mode) auto-mode-alist))
 
 ;; Emacs Speaks Statistics
-(setq load-path (cons "/usr/share/emacs/site-lisp/ess" load-path))
-(require 'ess-site)
+;; (setq load-path (cons "/usr/share/emacs/site-lisp/ess" load-path))
+;; (require 'ess-site)
 
 
+;; Ediff to split buffers side by side by default
 (setq ediff-split-window-function 'split-window-horizontally)
 
+;; Enable smart tabs mode
+(smart-tabs-insinuate 'c)
 (add-hook 'c-mode-hook
           (lambda ()
             (setq c-default "bsd"
-                  c-basic-offset 2
-                  indent-tabs-mode t)
-            (smart-tabs-mode-enable)
-            (smart-tabs-advice c-indent-line c-basic-offset)
-            (smart-tabs-advice c-indent-region c-basic-offset)))
+                  indent-tabs-mode t)))
